c2ecfd2773e45464a9e63809d06ec2a99e8a3f64
Zain <1155169521@link.cuhk.edu.hk>
2023-08-17 16:57:41 +0800

    update

--- src/CUHK_suction_place.py
+++ src/CUHK_suction_place.py
@@ -24,146 +24,128 @@ from helperFunction.SuctionP_callback_helper import P_CallbackHelp
 from helperFunction.fileSaveHelper import fileSaveHelp
 from helperFunction.adaptiveMotion import adaptMotionHelp
 from helperFunction.nachiHelper import NachiController
-
-rospy.init_node("suction_run")
-nachi_help = NachiController()
-grasp_info = SegmentationInfo()
-# experimental parameters
-timeLimit = 15
-# Setup helper functions
-P_help = P_CallbackHelp()  # it deals with subscription.
-rospy.sleep(0.5)
-file_help = fileSaveHelp()
-adapt_help = adaptMotionHelp(d_lat=5)
-end_workspace_limit = 950
-
-# Set data logger
-print("Wait for the data_logger to be enabled")
-rospy.wait_for_service("data_logging")
-dataLoggerEnable = rospy.ServiceProxy("data_logging", Enable)
-dataLoggerEnable(False)  # reset Data Logger just in case
-rospy.sleep(1)
-file_help.clearTmpFolder()  # clear the temporary folder
-
-print("Start sampling")
-# start sampling pressure , bias both
-P_help.startSampling()
-rospy.sleep(0.3)
+import argparse
 
 
-def init():
-    grasp_info_subscriber = rospy.Subscriber(
-        "nachi_left", SegmentationInfo, start_search
-    )
+class HapticSearchSync(object):
+    def __init__(self, arg):
+        rospy.init_node("suction_run")
+        self.nachi_help = NachiController()
+        self.grasp_info = SegmentationInfo()
+        # experimental parameters
+        self.P_help = P_CallbackHelp()  # it deals with subscription.
+        rospy.sleep(0.5)
+        self.file_help = fileSaveHelp()
+        self.adapt_help = adaptMotionHelp(d_lat=5)
 
+        # Set data logger
+        print("Wait for the data_logger to be enabled")
+        rospy.wait_for_service("data_logging")
+        self.dataLoggerEnable = rospy.ServiceProxy("data_logging", Enable)
 
-def start_search(msg):
-    dataLoggerEnable(True)  # start data logging
-    rospy.sleep(0.3)
-    # set biases now
-    try:
-        P_help.setNowAsOffset()
-        rospy.sleep(0.1)
-    except:
-        print("set now as offset failed, but it's okay")
-    grasp_info = msg
-    item_location = [
-        grasp_info.object_pose.pose.position.x * 1000,
-        grasp_info.object_pose.pose.position.y * 1000 + 30,
-        grasp_info.object_pose.pose.position.z * 1000,
-    ]
-    try:
-        waiting_point_y = -330
-        waiting_point = [item_location[0], waiting_point_y, 5 + 15]
-        nachi_help.move_robot_target_pose_sync(waiting_point)
+        self.dataLoggerEnable(False)  # reset Data Logger just in case
+        rospy.sleep(1)
+        self.file_help.clearTmpFolder()  # clear the temporary folder
+        print("Start sampling")
+        # start sampling pressure , bias both
+        self.P_help.startSampling()
+        rospy.sleep(0.3)
+        self.end_workspace_limit = 950
+        self.timeLimit = 15
+        self.waiting_point_y = -330
+        self.args = arg
+        self.waiting_point = [313, -65, 120]
+        self.grasp_info_subscriber = rospy.Subscriber(
+            "nachi_left", SegmentationInfo, self.start_search
+        )
 
-        # Target pose which is 15 mm above a PCB. For a test, use 50 mm above just in case
-        waiting_distance = waiting_point_y - item_location[1]
+    def start_search(self, msg):
+        self.dataLoggerEnable(True)
+        rospy.sleep(0.3)
+        try:
+            self.P_help.setNowAsOffset()
+            rospy.sleep(0.1)
+        except:
+            print("set now as offset failed, but it's okay")
+        grasp_info = msg
+        item_location = [
+            grasp_info.object_pose.pose.position.x * 1000,
+            grasp_info.object_pose.pose.position.y * 1000 + 30,
+            grasp_info.object_pose.pose.position.z * 1000,
+        ]
+        try:
+            waiting_point = [item_location[0], self.waiting_point_y, 5 + 15]
+            self.nachi_help.move_robot_target_pose_sync(waiting_point)
 
-        suctionFlag = False
-        pressureCheckFlag = False
-        vacuumCheckFlag = False
-        startTime = time.time()
-        iteration = 1
-        P_vac = adapt_help.P_vac
-        while (
-            nachi_help.conveyor_value - grasp_info.Register_value
-        ) < waiting_distance:
-            continue
-        current_value = nachi_help.conveyor_value
-        target_pose = [waiting_point[0], waiting_point[1], waiting_point[2] - 15]
-        while not suctionFlag:
+            # Target pose which is 15 mm above a PCB. For a test, use 50 mm above just in case
+            waiting_distance = self.waiting_point_y - item_location[1]
+
+            suction_flag = False
+            start_time = time.time()
+            iteration = 1
+            P_vac = self.adapt_help.P_vac
+            while (
+                self.nachi_help.conveyor_value - grasp_info.Register_value
+            ) < waiting_distance:
+                continue
+            current_value = self.nachi_help.conveyor_value
+            target_pose = [waiting_point[0], waiting_point[1], waiting_point[2] - 15]
+
             # move down
             target_pose[2] += -15
-            target_pose[1] = target_pose[1] + nachi_help.conveyor_value - current_value
-            current_value = nachi_help.conveyor_value
+            target_pose[1] = (
+                target_pose[1] + self.nachi_help.conveyor_value - current_value
+            )
+            current_value = self.nachi_help.conveyor_value
             print("iteration: ", iteration)
             print("move down")
-            if target_pose[1] > 200:
-                break
-            nachi_help.move_robot_target_pose_sync(target_pose)
+
+            self.nachi_help.move_robot_target_pose_sync(target_pose)
 
             # rospy.sleep(0.05)
-            P_check = P_help.four_pressure
+            P_check = self.P_help.four_pressure
 
             # move up
             target_pose[2] += +15
-            target_pose[1] = target_pose[1] + nachi_help.conveyor_value - current_value
-            current_value = nachi_help.conveyor_value
-            if target_pose[1] > 200:
-                break
-            nachi_help.move_robot_target_pose_sync(target_pose)
+            target_pose[1] = (
+                target_pose[1] + self.nachi_help.conveyor_value - current_value
+            )
+            current_value = self.nachi_help.conveyor_value
+
+            self.nachi_help.move_robot_target_pose_sync(target_pose)
             print("move up")
 
-            # Check vacuum & move to next pose
-            # rospy.sleep(0.05)
-            P = P_help.four_pressure
-            nachi_help.update_iteration(iteration)
-            if all(np.array(P) < P_vac):
-                print(f"Suction Engage Succeed from {iteration} touch")
-                suctionFlag = True
-                args.elapsedTime = startTime
-                break
-            elif time.time() - startTime > timeLimit:
-                args.timeOverFlag = True
-                break
-            else:
-                adapt_help.T = adapt_help.get_Tmat_lateralMove(P_check)
-                target_pose[0] += adapt_help.T[0, 3]
-                target_pose[1] += adapt_help.T[1, 3]
-                iteration += 1
-                # nachi_help.move_robot_target_pose_sync(targetPose)
+            rospy.sleep(1)
 
-        Waiting_point = [313, -65, 120]
-        nachi_help.move_robot_target_pose_sync(Waiting_point)
-        # Save args
-        args.suctionFlag = suctionFlag
-        args.iteration = iteration
-        args.timeLimit = timeLimit
-        # Save Init data
-        dataLoggerEnable(False)  # start data logging
-        # args = parser.parse_args()
-        file_help.saveDataParams(args, appendTxt="mode_" + str(args.mode))
-        file_help.clearTmpFolder()
-        P_help.stopSampling()
-        rospy.sleep(0.5)
+            place_postion = [35, 353, 119]
+            self.nachi_help.move_robot_target_pose_sync(place_postion)
 
-        print("==========Suction Pressure Test complete!==========")
+            rospy.sleep(1)
 
-    except rospy.ROSInterruptException:
-        return
-    except KeyboardInterrupt:
-        return
+            self.nachi_help.move_robot_target_pose_sync(self.waiting_point)
+            # Save args
+            self.args.suctionFlag = suction_flag
+            self.args.iteration = iteration
+            self.args.timeLimit = self.timeLimit
+            # Save Init data
+            self.dataLoggerEnable(False)  # start data logging
+            # args = parser.parse_args()
+            self.file_help.saveDataParams(
+                self.args, appendTxt="mode_" + str(self.args.mode)
+            )
+            self.file_help.clearTmpFolder()
+            self.P_help.stopSampling()
+            rospy.sleep(0.5)
 
+            print("==========Suction Pressure Test complete!==========")
 
-def main(args):
-    init()
-    rospy.spin()
+        except rospy.ROSInterruptException:
+            return
+        except KeyboardInterrupt:
+            return
 
 
 if __name__ == "__main__":
-    import argparse
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--mode",
@@ -175,4 +157,5 @@ if __name__ == "__main__":
         "--step", type=int, help="step size of haptic search", default=5
     )
     args = parser.parse_args()
-    main(args)
+    haptic_search_synv = HapticSearchSync(args)
+    rospy.spin()
