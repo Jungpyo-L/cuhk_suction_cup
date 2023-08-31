8cd0165a8601291f50601072ba1514b8c38ef488
1 <395282340@qq.com>
2023-08-31 17:10:36 +0800

    update

--- src/helperFunction/modbus_control.py
+++ src/helperFunction/modbus_control.py
@@ -4,17 +4,17 @@ from mighty_tools.common import timeit
 
 class ModbusController(object):
     def __init__(self):
-        self.client = ModbusSerialClient("/dev/ttyUSB1", baudrate=9600, timeout=0.1, parity="N")
+        self.client = ModbusSerialClient("/dev/ttyUSB0", baudrate=9600, timeout=0.1, parity="N")
         self.client.connect()
-
-    @timeit
-    def open_gas(self):
-        print("Gas is opened")
-        self.client.write_coil(0x00, True)
 
     @timeit
     def close_gas(self):
         print("Gas is closed")
+        self.client.write_coil(0x00, True)
+
+    @timeit
+    def open_gas(self):
+        print("Gas is opened")
         self.client.write_coil(0x00, False)
 
     @timeit
@@ -32,5 +32,6 @@ if __name__ == "__main__":
     gas_control = ModbusController()
     gas_control.stop_conveyor()
     # gas_control.open_gas()
+    # gas_control.close_gas()
     # gas_control.start_conveyor()
     print("a")
