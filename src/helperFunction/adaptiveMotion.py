0b1ffcde34c9fed5d579e812b252c3c83bc12ba8
Jungpyo Lee <87676982+Jungpyo-L@users.noreply.github.com>
2023-09-06 21:26:29 -0700

    changed vacuum threshold and haptic serach pressure threshold

--- src/helperFunction/adaptiveMotion.py
+++ src/helperFunction/adaptiveMotion.py
@@ -17,7 +17,7 @@ import scipy
 
 
 class adaptMotionHelp(object):
-    def __init__(self, dP_threshold=40, dw=15, P_vac=-10000, d_lat=5, d_z=1.5e-3):
+    def __init__(self, dP_threshold=18, dw=15, P_vac=-500, d_lat=5, d_z=1.5e-3):
         # for first performance test dw=15, d_lat = 0.5e-2, d_z= 1.5e-3
         # original dw = 3, d_lat = 0.1e-2, d_z = 0.3e-3
         self.dP_threshold = dP_threshold
